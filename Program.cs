2d7f3b7cad1384066ba1f0c113204e9256bb5b58
Fmorejon <morejonff@gmail.com>
2024-01-12 12:46:36 -0600

    finalresult

--- Program.cs
+++ Program.cs
@@ -8,17 +8,25 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
 if ((roll1 == roll2) || (roll2==roll3) || (roll1==roll3)){
     total += 2;
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
 }
+if ((roll1 == roll2) && (roll2==roll3)) {
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total+= 6;
+}
+
+Console.WriteLine($"Your Total is: {total}");
 
 if (total >= 15) {
-    Console.WriteLine("You win1");
+    Console.WriteLine("You win!");
 }
 if (total < 15) {
     Console.WriteLine("Sorry, you lose.");
 }
 
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
