c3a7eb6e7ed27626dfc3665376c12d62fe6614e4
Fmorejon <morejonff@gmail.com>
2024-01-12 15:16:49 -0600

    finished

--- obj/Debug/net8.0/aNewProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/aNewProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aNewProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70bab93277a4773c567c2acd29d50362e4c0292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45f5ba1d3032502f9b84ffff76a4a28a364302e")]
 [assembly: System.Reflection.AssemblyProductAttribute("aNewProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aNewProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
